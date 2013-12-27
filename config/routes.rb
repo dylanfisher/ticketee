acfe0d8f99b3cb5db25f10a934d25ef55a101bf5
dylanfisher <hi@dylanfisher.com>
2013-12-26 19:35:55 -0800

    Add admin namespaced users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Ticketee::Application.routes.draw do
     root 'projects#index'
 
+    namespace :admin do
+        resources :users
+    end
+
     get "/signin", to: "sessions#new"
     post "/signin", to: "sessions#create"
 
